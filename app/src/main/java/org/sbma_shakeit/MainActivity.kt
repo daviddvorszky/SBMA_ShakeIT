627057979bb90da972bcc99d56422d2d38a11b83
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-26 22:31:50 +0300

    Initial commit

---
+++ app/src/main/java/org/sbma_shakeit/MainActivity.kt
@@ -0,0 +1,30 @@
+package org.sbma_shakeit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sbma_shakeit.ui.theme.SBMA_ShakeITTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SBMA_ShakeITTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("Project Work (Shake IT)")
+                }
+            }
+        }
+    }
+}
