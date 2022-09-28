9c024dc0d84448fdba429db78d052d9242dbb98b
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-28 18:56:38 +0300

    Add buttons to move from new shake list screen to create shake screens

--- app/src/main/java/org/sbma_shakeit/screens/NewShakeListScreen.kt
+++ app/src/main/java/org/sbma_shakeit/screens/NewShakeListScreen.kt
@@ -1,12 +1,34 @@
 package org.sbma_shakeit.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import org.sbma_shakeit.navigation.Screen
 
 @Composable
 fun NewShakeListScreen(
     navController: NavController
 ) {
-    Text("New Shake List Screen")
+    Column(Modifier.fillMaxSize()) {
+        Text("New Shake List Screen")
+        Button(onClick = {
+            navController.navigate(Screen.QuickShake.route)
+        }) {
+            Text("Quick Shake")
+        }
+        Button(onClick = {
+            navController.navigate(Screen.LongShake.route)
+        }) {
+            Text("Long Shake")
+        }
+        Button(onClick = {
+            navController.navigate(Screen.ViolentShake.route)
+        }) {
+            Text("Violent Shake")
+        }
+    }
 }
