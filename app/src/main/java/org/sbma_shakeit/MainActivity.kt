e629d988ee993cb0c726e928e0677647db4c9880
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-28 18:50:19 +0300

    Add title bar and navigation to main activity

--- app/src/main/java/org/sbma_shakeit/MainActivity.kt
+++ app/src/main/java/org/sbma_shakeit/MainActivity.kt
@@ -3,26 +3,42 @@ package org.sbma_shakeit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.sbma_shakeit.components.TopMenuBar
+import org.sbma_shakeit.navigation.nav_graph.SetupNavGraph
 import org.sbma_shakeit.ui.theme.SBMA_ShakeITTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SBMA_ShakeITTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                navController = rememberNavController()
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    topBar = {
+                        TopMenuBar(navController = navController, "New Shake")
+                    }
                 ) {
-                    Text("Project Work (Shake IT)")
+                    innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)){
+                        SetupNavGraph(navController = navController)
+                    }
                 }
             }
         }
