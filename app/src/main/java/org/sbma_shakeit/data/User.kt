ed75714602684d8aa96ad98b8b57aff744690de3
Tatu <tatu.d.ihaksi@gmail.com>
2022-10-07 00:09:50 +0300

    Shake types

--- app/src/main/java/org/sbma_shakeit/data/User.kt
+++ app/src/main/java/org/sbma_shakeit/data/User.kt
@@ -1,9 +1,30 @@
 package org.sbma_shakeit.data
 
+import com.google.firebase.firestore.GeoPoint
+import java.util.*
+
 data class User(
     val username: String = "",
     val email: String = "",
-    val friends: List<String> = listOf()
+    val friends: List<String> = listOf(),
+    val quickShake: QuickShake = QuickShake(),
+    val violentShake: ViolentShake = ViolentShake(),
+    val longShake: LongShake = LongShake()
+)
+data class LongShake(
+    val time: Long = 0,
+    val created: Date = Date(),
+    val location: GeoPoint = GeoPoint(0.0, 0.0)
+)
+data class QuickShake(
+    val score: Int = 0,
+    val created: Date = Date(),
+    val location: GeoPoint = GeoPoint(0.0, 0.0)
+)
+data class ViolentShake(
+    val score: Float = 0.0F,
+    val created: Date = Date(),
+    val location: GeoPoint = GeoPoint(0.0, 0.0)
 )
 
 data class FriendRequest(
