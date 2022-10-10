ff66f3cbaa229d82602ddddbe2e8ca0730d92d36
Diego Turri <diego.webwork@gmail.com>
2022-10-10 16:50:56 +0300

    Created new Login UI

--- app/src/main/java/org/sbma_shakeit/screens/LoginScreen.kt
+++ app/src/main/java/org/sbma_shakeit/screens/LoginScreen.kt
@@ -2,20 +2,30 @@ package org.sbma_shakeit.screens
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import org.sbma_shakeit.R
 import org.sbma_shakeit.navigation.Screen
+import org.sbma_shakeit.ui.theme.Purple700
 import org.sbma_shakeit.viewmodels.AuthViewModel
 
 @Composable
@@ -25,29 +35,44 @@ fun LoginScreen(
 ) {
     val auth = authViewModel.auth.value
     val context = LocalContext.current
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val email = remember { mutableStateOf(TextFieldValue()) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
     var showPassword by remember { mutableStateOf(false) }
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        ClickableText(
+            text = AnnotatedString("Sign up here"),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(20.dp),
+            onClick = { navController.navigate(Screen.Register.route) },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                textDecoration = TextDecoration.Underline,
+                color = Purple700
+            )
+        )
+    }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { navController.navigate(Screen.Register.route) }) {
-            Text("To register")
-        }
-        TextField(
-            label = { Text("Email") },
-            value = email,
-            onValueChange = { email = it })
+        Text(text = "Login", style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive))
 
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
-            label = { Text("Password") },
-            value = password,
-            onValueChange = { password = it },
+            label = { Text(text = "Email") },
+            value = email.value,
+            onValueChange = { email.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
             visualTransformation = if (showPassword) VisualTransformation.None
-                else PasswordVisualTransformation(),
+            else PasswordVisualTransformation(),
             trailingIcon = {
                 IconButton(onClick = {
                     showPassword = !showPassword
@@ -63,24 +88,44 @@ fun LoginScreen(
                             contentDescription = "Show password"
                         )
                 }
-            }
-        )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
 
-        Button(onClick = {
-            auth ?: return@Button
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Log.d("LOGIN", "SUCCESS")
-                        navController.navigate(Screen.NewShakeList.route)
-                    } else {
-                        Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
-                        Log.d("LOGIN", "FAILED", it.exception)
-                    }
-                }
-        }) {
-            Text("Sign in")
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {
+                    auth ?: return@Button
+                    auth.signInWithEmailAndPassword(email.value.text, password.value.text)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Log.d("LOGIN", "SUCCESS")
+                                navController.navigate(Screen.NewShakeList.route)
+                            } else {
+                                Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
+                                Log.d("LOGIN", "FAILED", it.exception)
+                            }
+                        }
+                },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Login")
+            }
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default
+            )
+        )
     }
 }
 
