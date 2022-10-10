a42f7ef2ead0c7267fb09247fa2fcdc16b08e970
Diego Turri <diego.webwork@gmail.com>
2022-10-10 17:23:44 +0300

    Created new Register UI

--- app/src/main/java/org/sbma_shakeit/screens/RegisterScreen.kt
+++ app/src/main/java/org/sbma_shakeit/screens/RegisterScreen.kt
@@ -2,21 +2,31 @@ package org.sbma_shakeit.screens
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import org.sbma_shakeit.R
 import org.sbma_shakeit.data.room.User
 import org.sbma_shakeit.navigation.Screen
+import org.sbma_shakeit.ui.theme.Purple700
 import org.sbma_shakeit.viewmodels.AuthViewModel
 
 @Composable
@@ -26,32 +36,49 @@ fun RegisterScreen(
 ) {
     val auth = authViewModel.auth.value
     val context = LocalContext.current
-    var email by remember { mutableStateOf("") }
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val email = remember { mutableStateOf(TextFieldValue()) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
+    val username = remember { mutableStateOf(TextFieldValue()) }
     var showPassword by remember { mutableStateOf(false) }
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        ClickableText(
+            text = AnnotatedString("Back to login"),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(20.dp),
+            onClick = { navController.navigate(Screen.Login.route) },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                textDecoration = TextDecoration.Underline,
+                color = Purple700
+            )
+        )
+    }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center)
-    {
-        Button(onClick = { navController.navigate(Screen.Login.route) }) {
-            Text("To login")
-        }
-        TextField(
-            label = { Text("Email") },
-            value = email,
-            onValueChange = { email = it })
-        TextField(
-            label = { Text("Username") },
-            value = username,
-            onValueChange = { username = it })
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Register", style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive))
 
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
-            label = { Text("Password") },
-            value = password,
-            onValueChange = { password = it },
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Email") },
+            value = email.value,
+            onValueChange = { email.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
             visualTransformation = if (showPassword) VisualTransformation.None
             else PasswordVisualTransformation(),
             trailingIcon = {
@@ -61,7 +88,7 @@ fun RegisterScreen(
                     if (showPassword)
                         Icon(
                             painterResource(R.drawable.visibility_off),
-                            contentDescription = "Show password"
+                            contentDescription = "Don't show password"
                         )
                     else
                         Icon(
@@ -69,31 +96,41 @@ fun RegisterScreen(
                             contentDescription = "Show password"
                         )
                 }
-            }
-        )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
 
-        Button(onClick = {
-            auth ?: return@Button
-            val isUserNameTaken = authViewModel.isUsernameTaken(username)
-            Log.d("isUsernameTaken", "$isUserNameTaken")
-            if (isUserNameTaken) {
-                Toast.makeText(context, "Username taken", Toast.LENGTH_LONG).show()
-                return@Button
-            }
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        authViewModel.createUser(User(username, email))
-                        Log.d("REGISTER", "SUCCESS")
-                        navController.navigate(Screen.NewShakeList.route)
-                    } else {
-                        val message = it.exception?.message ?: "Register failed"
-                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-                        Log.d("REGISTER", "FAILED", it.exception)
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {
+                    auth ?: return@Button
+                    val isUserNameTaken = authViewModel.isUsernameTaken(username.value.text)
+                    Log.d("isUsernameTaken", "$isUserNameTaken")
+                    if (isUserNameTaken) {
+                        Toast.makeText(context, "Username taken", Toast.LENGTH_LONG).show()
+                        return@Button
                     }
-                }
-        }) {
-            Text("Sign Up")
+                    auth.createUserWithEmailAndPassword(email.value.text, password.value.text)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                authViewModel.createUser(User(username.value.text, email.value.text))
+                                Log.d("REGISTER", "SUCCESS")
+                                navController.navigate(Screen.NewShakeList.route)
+                            } else {
+                                val message = it.exception?.message ?: "Register failed"
+                                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                                Log.d("REGISTER", "FAILED", it.exception)
+                            }
+                        }
+                },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Sign up")
+            }
         }
     }
 }
