df2f84e6078a1d09536d0028b87e86d66ec011bc
Diego Turri <diego.webwork@gmail.com>
2022-10-12 18:13:30 +0300

    scoreboard UI general check of the Theme related objects

--- app/src/main/java/org/sbma_shakeit/components/TabsView.kt
+++ app/src/main/java/org/sbma_shakeit/components/TabsView.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,7 +36,7 @@ fun TabsView(selectedIndex: MutableState<Int>, headers: List<String>) {
                 selectedContentColor = Color.Black,
                 unselectedContentColor = Color.Black
             ) {
-                Text(header)
+                Text(header, fontWeight = FontWeight.Bold)
             }
         }
     }
