b9d831492e4d5759ef7fc01a0a6bb536f241041b
daviddvorszky <dv98david@gmail.com>
2022-10-12 22:34:18 +0300

    Add application, activity, and database to navigation graph

--- app/src/main/java/org/sbma_shakeit/navigation/nav_graph/NavGraph.kt
+++ app/src/main/java/org/sbma_shakeit/navigation/nav_graph/NavGraph.kt
@@ -1,11 +1,14 @@
 package org.sbma_shakeit.navigation.nav_graph
 
+import android.app.Activity
+import android.app.Application
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.sbma_shakeit.data.room.ShakeItDB
 import org.sbma_shakeit.navigation.AUTH_GRAPH_ROUTE
 import org.sbma_shakeit.navigation.NEW_SHAKE_GRAPH_ROUTE
 import org.sbma_shakeit.navigation.ROOT_GRAPH_ROUTE
@@ -16,7 +19,10 @@ import org.sbma_shakeit.viewmodels.users.AuthViewModel
 @Composable
 fun SetupNavGraph(
     navController: NavHostController,
-    authViewModel: AuthViewModel
+    authViewModel: AuthViewModel,
+    application: Application,
+    activity: Activity,
+    database: ShakeItDB
 ) {
     val startDestination =
         if (Firebase.auth.currentUser != null) NEW_SHAKE_GRAPH_ROUTE
@@ -27,7 +33,12 @@ fun SetupNavGraph(
         startDestination = startDestination,
         route = ROOT_GRAPH_ROUTE
     ) {
-        newShakeNavGraph(navController = navController)
+        newShakeNavGraph(
+            navController,
+            application,
+            activity,
+            database
+        )
         historyNavGraph(navController = navController)
         scoreboardNavGraph(navController = navController)
         userNavGraph(navController = navController)
