49548dac29fbc57c4964e4c00d4034c1a9969b20
Diego Turri <diego.webwork@gmail.com>
2022-10-13 12:07:56 +0300

    Map feauture home screen

--- app/src/main/java/org/sbma_shakeit/screens/NewShakeListScreen.kt
+++ app/src/main/java/org/sbma_shakeit/screens/NewShakeListScreen.kt
@@ -1,18 +1,36 @@
 package org.sbma_shakeit.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.app.Activity
+import android.app.Application
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.sbma_shakeit.MainActivity
+import org.sbma_shakeit.R
 import org.sbma_shakeit.navigation.Screen
+import org.sbma_shakeit.viewmodels.LocationViewModel
 
 @Composable
 fun NewShakeListScreen(
     navController: NavController
 ) {
+
+    //MAP VARIABLES
+    val locationViewModel = LocationViewModel(application = Application(), Activity(), MainActivity.lm)
+
     Column(Modifier.fillMaxSize()) {
         Text("New Shake List Screen")
         Button(onClick = {
@@ -30,5 +48,53 @@ fun NewShakeListScreen(
         }) {
             Text("Violent Shake")
         }
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(
+            elevation = 10.dp,
+            backgroundColor = MaterialTheme.colors.background,
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            ShowMap(locationViewModel = locationViewModel, navController)
+        }
     }
 }
+
+
+@Composable
+fun composeMap(): MapView {
+    val context = LocalContext.current
+    val mapView = remember{
+        MapView(context).apply { id = R.id.map }
+    }
+    return mapView
+}
+
+@Composable
+fun ShowMap(locationViewModel: LocationViewModel, navController: NavController){
+    val map = composeMap()
+    var mapInizialized by remember(map){ mutableStateOf(false) }
+    val marker = Marker(map)
+    val currentGeoPoint = locationViewModel.currentGeoPoint.observeAsState()
+
+
+    if (!mapInizialized){
+        map.setTileSource(TileSourceFactory.MAPNIK)     //Set the Tiles source
+        map.setMultiTouchControls(true)                 //Ability to zoom with 2 fingers
+        map.controller.setZoom(9.0)                     //Set the default zoom
+        map.controller.setCenter(GeoPoint(60.0, 25.0)) //set the center of the map initialization
+
+        mapInizialized = true
+    }
+    AndroidView({map}){
+        currentGeoPoint ?: return@AndroidView
+        it.controller.setCenter(currentGeoPoint.value)
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.position = currentGeoPoint.value
+        marker.closeInfoWindow()
+        marker.title = "You are here! latitude: "+currentGeoPoint.value?.latitude+" longitude: "+currentGeoPoint.value?.longitude
+        map.overlays.add(marker)
+        map.invalidate()
+    }
+}
+
--- app/src/main/java/org/sbma_shakeit/screens/UserProfileScreen.kt
+++ app/src/main/java/org/sbma_shakeit/screens/UserProfileScreen.kt
@@ -1,12 +1,19 @@
 package org.sbma_shakeit.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import org.sbma_shakeit.components.FriendRequestList
 import org.sbma_shakeit.viewmodels.users.UserViewModel
 
 @Composable
@@ -19,13 +26,25 @@ fun UserProfileScreen(
 //    val date = userData.quickShake.created
 //    val f = SimpleDateFormat("dd.MM.yyyy").format(date)
 
-    Column {
-        // TODO: Profile data
-        Text("User is ${userData.username}")
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Card(
+            elevation = 10.dp,
+            backgroundColor = MaterialTheme.colors.background,
+            modifier = Modifier
+                .fillMaxSize(0.95f)
+        ) {
+            Text(userData.username, fontWeight = FontWeight.Bold)
+
+        }
+    }
+}
+
 //        Text("date $f")
 //        Text("Long shake time ${userData.longShake.time}s")
 //        Text("Violent shake score ${userData.violentShake.score}")
 //        Text("Quick shake score ${userData.quickShake.score}")
-        FriendRequestList()
-    }
-}
+//            FriendRequestList()
